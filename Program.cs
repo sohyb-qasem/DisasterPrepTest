bba813505b079f12ebf2c902bb44f32b3882a3f3
Sohyb Qasem <sohybqasem@gmail.com>
2023-05-19 13:19:37 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,34 @@
-using System.IO;
-using System.Threading.Tasks;
-
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", async () => {
-    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "index.html");
-    using var streamReader = new StreamReader(filePath);
-    var content = await streamReader.ReadToEndAsync();
-    return Results.Content(content, "text/html");
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+// Serve static files and set the default file to index.html
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.MapFallback(context =>
+{
+    context.Response.Redirect("/index.html");
+    return Task.CompletedTask;
 });
 
 app.Run();
